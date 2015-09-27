b9d98db0fc6c977b0dabde36e5cbde7a3bfabc39
Veyza <Veyza@users.noreply.github.com>
2015-09-27 15:51:32 +0300

    The first and hope that last version of functions makeCacheMatrix and cacheSolve for peer assessment R programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## functions allow to avoid extra computations of inverse matrixes
+## the inverse matrix received once is keeping in cache untill
+## the initial matrix changes
 
-## Write a short comment describing this function
+## The makeCacheMatrix create a list of functions 
+## for setting and getting a matrix and its inverse
 
-makeCacheMatrix <- function(x = matrix()) {
+    makeCacheMatrix <- function(A = matrix()) {
+	
+	inv <- NULL
+	
+	set <- function(B) {
+	    A <<- B
+	    inv <<- NULL
+	}
+	get <-function() A
+	setInv <- function(inverse) inv <<- inverse
+	getInv <- function() inv
+	
+	list(set =set, get = get, setInv = setInv, getInv = getInv)
+    
+    }
+    
+    
 
-}
+## The cacheSolve get a list of functions described above, 
+## return a matrix that is inverse of A
+## the function would compute the inverse only if A changed
+## else the function take inverse A from cache
 
+    cacheSolve <- function(x, ...) {
+        
+        inv <- x$getInv()
+	
+        if(!is.null(inv)) {
+	    message("getting cached data")
+	    return(inv)
+        }
+    
+	M <- x$get()
+        inv <- solve(M)
+        x$setInv(inv)
+    
+	inv
 
-## Write a short comment describing this function
+    }
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
